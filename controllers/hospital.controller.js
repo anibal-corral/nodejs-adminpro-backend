fffb1260033b014be789b3faf8b172e850dd555f
Anibal Corral <anibal.corral@gmail.com>
2022-10-27 10:18:42 -0400

    Update Hospital implemented

--- controllers/hospital.controller.js
+++ controllers/hospital.controller.js
@@ -50,11 +50,10 @@ const getHospitals =  async (req, res)=>{
 
 
 const updateHospital=async(req,res)=>{
-    const uid = req.params.id;
-    
-    console.log(uid);
+    const id = req.params.id;
+    const uid = req.uid;
     try {
-        const hospitalDB = await Hospital.findById(uid);
+        const hospitalDB = await Hospital.findById(id);
         if(!hospitalDB){
             return res.status(404).json({
                 ok:false,
@@ -62,29 +61,16 @@ const updateHospital=async(req,res)=>{
             })
         }
         //Update
-        const fields = req.body;
-        delete fields.pwd;
-        delete fields.google;
-
-        if(hospitalDB.email === req.body.email){
-            delete fields.email;
-        }else{
-            const checkEmail = await Hospital.findOne({email:req.body.email});
-            if(checkEmail){
-                return res.status(400).json({
-                    ok:false,
-                    msg:"Email already exists."
-                })
-            }
+        const changesHospital = {
+            ...req.body,
+            user:uid
         }
-
-
-        const hospitalUpdated = await Hospital.findByIdAndUpdate(uid, fields, {new: true});
+        const hospitalUpdated = await Hospital.findByIdAndUpdate(id, changesHospital, {new: true});
 
 
         res.status(200).json({
             ok:true,
-            hospitalUpdated
+            hospital:hospitalUpdated
         })
     } catch (error) {
         console.log(error);
@@ -108,8 +94,6 @@ const updateHospital=async(req,res)=>{
             })
         }
         await Hospital.findByIdAndDelete(uid);
-
-
         res.status(200).json({
             ok:true,
             msg:"Hospital deleted"
